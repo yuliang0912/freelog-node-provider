b2d35f34b7935d28faa243999801cd43f5083462
yuliang <yu.liang@freelog.com>
2018-01-19 16:41:28 +0800

    1.修改路由.解决egg2.0升级问题

--- app/router.js
+++ app/router.js
@@ -6,11 +6,6 @@
 module.exports = app => {
 
     /**
-     * node-pb restful api
-     */
-    app.resources('/v1/nodes/pagebuilds', '/v1/nodes/pagebuilds', app.controller.nodePageBuild.v1)
-
-    /**
      * 创建pb的presentable
      */
     app.post('/v1/presentables/createPageBuildPresentable', app.controller.presentable.v1.createPageBuildPresentable)
@@ -19,21 +14,15 @@ module.exports = app => {
      * 关联pb-persentable对应的插件合同ID
      */
     app.post('/v1/presentables/pageBuildAssociateWidget', app.controller.presentable.v1.pageBuildAssociateWidget)
-
     app.get('/v1/presentables/pageBuildAssociateWidgetContract', app.controller.presentable.v1.pageBuildAssociateWidgetContract)
     app.get('/v1/presentables/pageBuildAssociateWidgetPresentable', app.controller.presentable.v1.pageBuildAssociateWidgetPresentable)
     app.get('/v1/presentables/pbPresentableStatistics', app.controller.presentable.v1.pbPresentableStatistics)
 
     /**
-     * presentables restful api
-     */
-    app.resources('/v1/presentables', '/v1/presentables', app.controller.presentable.v1)
-
-    /**
      * node主页相关路由
      */
     //app.get(/^\/node\/([a-zA-Z0-9-]{4,24}[\/]?)$/, app.middlewares.nodeDomainAuth(), app.controller.node.home.index)
-    app.get('/node/:nodeDomain', app.middlewares.nodeDomainAuth(), app.controller.node.home.index)
+    //app.get('/node/:nodeDomain', app.middlewares.nodeDomainAuth(), app.controller.node.home.index)
     //暂时先不要 以后节点渲染站点单独独立出来
     //app.get('/node/:nodeDomain/presentable/:presentableId.:extName', app.middlewares.nodeDomainAuth(), app.controller.node.home.presentableResource)
     //app.get('/node/:nodeDomain/presentable/:presentableId', app.middlewares.nodeDomainAuth(), app.controller.node.home.presentableResource)
@@ -43,16 +32,25 @@ module.exports = app => {
     app.get('/v1/nodes/:nodeId/presentables/:presentableId', app.controller.presentable.auth.resource)
 
     /**
+     * index
+     */
+    app.redirect('/', '/public/index.html', 404);
+
+    /**
+     * node-pb restful api
+     */
+    app.resources('/v1/nodes/pagebuilds', '/v1/nodes/pagebuilds', app.controller.nodePageBuild.v1)
+
+    /**
      * node restful api
      */
     app.resources('/v1/nodes', '/v1/nodes', app.controller.node.v1)
 
-    app.get('/node/home', app.controller.node.home.index1)
-
     /**
-     * index
+     * presentables restful api
      */
-    app.redirect('/', '/public/index.html', 404);
+    app.resources('/v1/presentables', '/v1/presentables', app.controller.presentable.v1)
+
 }
 
 
