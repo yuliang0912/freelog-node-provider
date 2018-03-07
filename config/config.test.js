7befd3ebe308c941d33c6f2645372d408d194ae6
yuliang <yu.liang@freelog.com>
2018-03-07 10:29:15 +0800

    1.修改配置

--- config/config.test.js
+++ config/config.test.js
@@ -4,13 +4,26 @@
 
 'use strict'
 
-module.exports = appInfo => {
-    return {
-
-        gatewayUrl: "http://192.168.164.129",
-
-        mongoose: {
-            uri: "mongodb://192.168.0.99:27017/node"
-        },
+module.exports = {
+    /**
+     * DB-mysql相关配置
+     */
+    knex: {
+        node: {
+            connection: {
+                host: 'rm-wz9wj9435a0428942.mysql.rds.aliyuncs.com',
+                user: 'freelog',
+                password: 'Ff@233109',
+                database: 'fr_node'
+            },
+            debug: false
+        }
+    },
+    
+    /**
+     * mongodb连接
+     */
+    mongoose: {
+        url: "mongodb://root:Ff233109@dds-wz9b5420c30a27941546-pub.mongodb.rds.aliyuncs.com:3717,dds-wz9b5420c30a27942267-pub.mongodb.rds.aliyuncs.com:3717/node?replicaSet=mgset-5016983"
     }
 }
