ba9f3d0d6d82ba1a73c17571e82b199caf6c0fc2
yuliang <yu.liang@freelog.com>
2020-03-27 14:16:03 +0800

    1.修改i18n目录结构

--- config/locale/en-US.js
+++ config/locale/en-US.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const i18nEnUsNodeText = require('../i18n-text/en-US/node')
-const i18nEnUsCommonText = require('../i18n-text/en-US/common')
+const i18nEnUsNodeText = require('../i18n-text/freelog-node-provider/en-US/node')
+const i18nEnUsCommonText = require('../i18n-text/freelog-node-provider/en-US/common')
 
 module.exports = Object.assign(i18nEnUsNodeText, i18nEnUsCommonText)
---
+++ config/locale/zh-CN.js
@@ -0,0 +1,6 @@
+'use strict'
+
+const i18nEnUsNodeText = require('../i18n-text/freelog-node-provider/zh-CN/node')
+const i18nEnUsCommonText = require('../i18n-text/freelog-node-provider/zh-CN/common')
+
+module.exports = Object.assign(i18nEnUsNodeText, i18nEnUsCommonText)
