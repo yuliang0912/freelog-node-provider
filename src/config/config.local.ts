2708f7e973f335cc21993a8bea5a87934648e926
yuliang <yu.liang@freelog.com>
2021-05-11 10:36:42 +0800

    1.提交代码

--- src/config/config.local.ts
+++ src/config/config.local.ts
@@ -21,13 +21,16 @@ export default () => {
         url: `mongodb://node_service:MTAwZGRhODU0Njc2MTM=@dds-wz9ac40fee5c09441604-pub.mongodb.rds.aliyuncs.com:3717,dds-wz9ac40fee5c09442584-pub.mongodb.rds.aliyuncs.com:3717/local-nodes?replicaSet=mgset-44484047`
     };
 
-    // config.gatewayUrl = 'http://api.testfreelog.com';
+    config.mongoose = {
+        url: `mongodb://node_service:QzA4Qzg3QTA3NDRCQTA0NDU1RUQxMjI3MTA4ODQ1MTk=@dds-wz9ac40fee5c09441604-pub.mongodb.rds.aliyuncs.com:3717,dds-wz9ac40fee5c09442584-pub.mongodb.rds.aliyuncs.com:3717/test-nodes?replicaSet=mgset-44484047`,
+    };
+    config.gatewayUrl = 'http://api.testfreelog.com';
     // config.mongoose = {
     //     url: ''
     // };
 
     config.localIdentity = {
-        userId: 50021,
+        userId: 50031,
         username: 'yuliang',
         email: 'support@freelog.com'
     };
