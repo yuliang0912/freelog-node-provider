5bdbd8983a566ef1a3cf369388f19afeaa274848
yuliang <4896819@qq.com>
2022-01-25 14:40:23 +0800

    1.提交代码

--- dist/config/config.prod.js
+++ dist/config/config.prod.js
@@ -3,11 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = () => {
     const config = {};
     config.mongoose = {
-        url: 'mongodb://mongo-prod.common:27017/node-beta'
+        url: decodeURIComponent(`mongodb%3A%2F%2Fnode_service%3AQzA4Qzg3QTA3NDRCQTA0NDU1RUQxMjI3MTA4ODQ1MTk%3D%40freelog-prod-private.mongodb.rds.aliyuncs.com%3A3717%2Cfreelog-prod-private-secondary.mongodb.rds.aliyuncs.com%3A3717%2Fprod-nodes%3FreplicaSet%3Dmgset-58730021`)
     };
-    // config.mongoose = {
-    //     url: 'mongodb://39.108.77.211:30772/node-beta'
-    // };
+    config.kafka = {
+        enable: false,
+        clientId: 'freelog-node-service',
+        logLevel: 1,
+        brokers: ['kafka-svc.common:9093']
+    };
     return config;
 };
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29uZmlnLnByb2QuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvY29uZmlnL2NvbmZpZy5wcm9kLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUEsa0JBQWUsR0FBRyxFQUFFO0lBQ2hCLE1BQU0sTUFBTSxHQUFRLEVBQUUsQ0FBQztJQUV2QixNQUFNLENBQUMsUUFBUSxHQUFHO1FBQ2QsR0FBRyxFQUFFLDZDQUE2QztLQUNyRCxDQUFDO0lBRUYsc0JBQXNCO0lBQ3RCLHFEQUFxRDtJQUNyRCxLQUFLO0lBRUwsT0FBTyxNQUFNLENBQUM7QUFDbEIsQ0FBQyxDQUFDIn0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29uZmlnLnByb2QuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvY29uZmlnL2NvbmZpZy5wcm9kLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUEsa0JBQWUsR0FBRyxFQUFFO0lBQ2hCLE1BQU0sTUFBTSxHQUFRLEVBQUUsQ0FBQztJQUV2QixNQUFNLENBQUMsUUFBUSxHQUFHO1FBQ2QsR0FBRyxFQUFFLGtCQUFrQixDQUFDLGtQQUFrUCxDQUFDO0tBQzlRLENBQUM7SUFFRixNQUFNLENBQUMsS0FBSyxHQUFHO1FBQ1gsTUFBTSxFQUFFLEtBQUs7UUFDYixRQUFRLEVBQUUsc0JBQXNCO1FBQ2hDLFFBQVEsRUFBRSxDQUFDO1FBQ1gsT0FBTyxFQUFFLENBQUMsdUJBQXVCLENBQUM7S0FDckMsQ0FBQztJQUVGLE9BQU8sTUFBTSxDQUFDO0FBQ2xCLENBQUMsQ0FBQyJ9
--- src/config/config.prod.ts
+++ src/config/config.prod.ts
@@ -2,12 +2,15 @@ export default () => {
     const config: any = {};
 
     config.mongoose = {
-        url: 'mongodb://mongo-prod.common:27017/node-beta'
+        url: decodeURIComponent(`mongodb%3A%2F%2Fnode_service%3AQzA4Qzg3QTA3NDRCQTA0NDU1RUQxMjI3MTA4ODQ1MTk%3D%40freelog-prod-private.mongodb.rds.aliyuncs.com%3A3717%2Cfreelog-prod-private-secondary.mongodb.rds.aliyuncs.com%3A3717%2Fprod-nodes%3FreplicaSet%3Dmgset-58730021`)
     };
 
-    // config.mongoose = {
-    //     url: 'mongodb://39.108.77.211:30772/node-beta'
-    // };
+    config.kafka = {
+        enable: false,
+        clientId: 'freelog-node-service',
+        logLevel: 1, // logLevel.ERROR,
+        brokers: ['kafka-svc.common:9093']
+    };
 
     return config;
 };
