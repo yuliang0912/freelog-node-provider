e8cbf3629e2b80b742cb1800170ac462bd127b24
yuliang <4896819@qq.com>
2022-01-26 11:27:26 +0800

    1.提交代码

--- src/config/config.prod.ts
+++ src/config/config.prod.ts
@@ -1,6 +1,10 @@
 export default () => {
     const config: any = {};
 
+    config.cluster = {
+        listen: {port: 7105}
+    };
+
     config.mongoose = {
         url: decodeURIComponent(`mongodb%3A%2F%2Fnode_service%3AQzA4Qzg3QTA3NDRCQTA0NDU1RUQxMjI3MTA4ODQ1MTk%3D%40freelog-prod-private.mongodb.rds.aliyuncs.com%3A3717%2Cfreelog-prod-private-secondary.mongodb.rds.aliyuncs.com%3A3717%2Fprod-nodes%3FreplicaSet%3Dmgset-58730021`)
     };
