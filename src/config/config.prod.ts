7904bbb328345be8ced2586eabbe7ab9aaaac107
yuliang <4896819@qq.com>
2022-04-13 11:09:43 +0800

    1.提交代码

--- src/config/config.prod.ts
+++ src/config/config.prod.ts
@@ -13,7 +13,7 @@ export default () => {
         enable: true,
         clientId: 'freelog-node-service',
         logLevel: 1, // logLevel.ERROR,
-        brokers: ['kafka-hs.production:9092'],
+        brokers: ['kafka-0.production:9092', 'kafka-1.production:9092', 'kafka-2.production:9092'],
         connectionTimeout: 3000,
         retry: {
             initialRetryTime: 5000,
